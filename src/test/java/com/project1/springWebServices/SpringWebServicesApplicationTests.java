5d5852acd1c54c58b1dd1b3f36af435be228a1c0
Israel Machado <israelmach26@gmail.com>
2023-03-30 08:10:10 -0300

    Project created

---
+++ src/test/java/com/project1/springWebServices/SpringWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1.springWebServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
